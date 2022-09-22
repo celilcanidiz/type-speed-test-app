0a6d6760eda3df35e4dc9fbef41c915ee6b88953
celilcanidiz <celilcanidiz@gmail.com>
2022-09-22 14:47:34 +0300

    Revert "add provider :d"
    This reverts commit 3a96393a50f6ededf70a79f7abc614243535bd13.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Main from './components/Main';
-import { Provider } from 'react-redux';
-import {store} from './redux/store'
+
 function App() {
   return (
-    <Provider store={store}> 
     <Main />
-    </Provider>
   );
 }
 
