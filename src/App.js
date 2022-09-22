3a96393a50f6ededf70a79f7abc614243535bd13
celilcanidiz <celilcanidiz@gmail.com>
2022-09-22 14:42:46 +0300

    add provider :d

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Main from './components/Main';
-
+import { Provider } from 'react-redux';
+import {store} from './redux/store'
 function App() {
   return (
+    <Provider store={store}> 
     <Main />
+    </Provider>
   );
 }
 
